82818809e7dc6483be7c1d9d1226ce69c0493c6c
Reimon Ferreira <reimonf@gmail.com>
2020-07-15 07:34:54 -0300

    :rocket: Criando o primeiro teste 

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,7 @@
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 export default interface IAppointmentsRepository {
-  create?(): Promise<Appointment>;
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
