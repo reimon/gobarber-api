e28852110a46f4260d875a945b1274ecd8e6a34e
Reimon Ferreira <reimonf@gmail.com>
2020-07-20 14:58:49 -0300

    :rocket: Regras de agendamento 

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -27,14 +27,19 @@ class CreateAppointmentService {
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
-      appointmentDate
-    );
-
-    if (findAppointmentInSameDate) {
-      throw new AppError('This appointment is already booked');
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date.");
     }
 
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself.");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'You can only create appointments between 8am and 5pm'
+      );
+    }
     const appointment = await this.appointmentsRepository.create({
       provider_id,
       user_id,
