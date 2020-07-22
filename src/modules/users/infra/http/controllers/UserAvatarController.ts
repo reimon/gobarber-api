cd6bb1a503dbeea6680739718d92dc4f0cca04b8
Reimon Ferreira <reimonf@gmail.com>
2020-07-22 14:41:18 -0300

    :rocket: Utilizando Class Transformer

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateUserAvatar from '@modules/users/services/UpdateUserAvatarService';
 
@@ -15,7 +16,7 @@ export default class UsersAvatarController {
 
       delete user.password;
 
-      return response.json(user);
+      return response.json(classToClass(user));
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
