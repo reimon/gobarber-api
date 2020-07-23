1a82fcdbb9e7f99508f4f5a1aa4e500c4541e40e
Reimon Ferreira <reimonf@gmail.com>
2020-07-23 07:26:44 -0300

    Organizando o Container

--- src/shared/container/providers/MailTemplateProvider/index.ts
+++ src/shared/container/providers/MailTemplateProvider/index.ts
@@ -1,9 +1,14 @@
 import { container } from 'tsyringe';
 
 import IMailTemplateProvider from './models/IMailTemplateProvider';
+
 import HandlebarsMailTemplateProvider from './implementations/HandlebarsMailTemplateProvider';
 
+const providers = {
+  handlebars: HandlebarsMailTemplateProvider,
+};
+
 container.registerSingleton<IMailTemplateProvider>(
-  'IMailTemplateProvider',
-  HandlebarsMailTemplateProvider
+  'MailTemplateProvider',
+  providers.handlebars
 );
